257fbd497ff4183acfadc5977d2f5756007490c0
yongukmoon <yongukmoon@naver.com>
2019-07-01 18:17:23 +0900

    Navigation generate

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,21 @@
 @extends('layouts.master')
 
+@section('style')
+<link href="https://fonts.googleapis.com/css?family=Poiret+One&display=swap" rel="stylesheet">
+
+    <style>
+        .navbar {
+            margin: 0.5em;
+        }
+        .main_title {
+            color:black;
+            font-size: 5em;
+            text-align: center;
+            font-family: 'Poiret One', cursive;
+        }
+    </style>
+@endsection
+
 @section('content')
-    
+    <h1 class="main_title">Yonguk Moon Portfolio</h1>
 @endsection
